79e367023ba3736d0f682e3ee470008e03aa66c9
Madhur Jain <30556358+madhur02@users.noreply.github.com>
2025-07-12 23:34:48 +0530

    Update tfidf_tSNE.py

--- tfidf_tSNE.py
+++ tfidf_tSNE.py
@@ -1,10 +1,7 @@
-# Course URL:
-# https://deeplearningcourses.com/c/natural-language-processing-with-deep-learning-in-python
-# https://udemy.com/natural-language-processing-with-deep-learning-in-python
 from __future__ import print_function, division
 from future.utils import iteritems
 from builtins import range
-# Note: you may need to update your version of future
+
 # sudo pip install -U future
 
 
